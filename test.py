ab7888bc79c23dcd16e007c1cb8846327ebf2330
jonahboe <jonahwb@byui.edu>
2020-06-24 12:05:30 -0600

    removed uneeded GPS stuf

--- test.py
+++ test.py
@@ -26,8 +26,8 @@ while True:
           else:
                print("Latitude:", data[3], data[4])
                print("Longitude:", data[5], data[6])
-               print("Speed: ", data[7],)
-               print("Course: ", data[8], '\n')
+               # print("Speed: ", data[7],)
+               # print("Course: ", data[8], '\n')
 
      except:
           print("There was an error.\n")
